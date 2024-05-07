e42cc0e505c40b7ad6ef0215ae8c5c4247ccdeb8
tharunisr <tharuniradhakrishnan27@gmail.com>
2024-05-07 11:51:34 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TallySoftware.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
